d1d8994f8b529a15bb39309cff3fbd1144288f32
Haneesh Bandaru <97587849+haneesh-bandaru@users.noreply.github.com>
2024-01-29 13:00:51 +0000

    updated images in carousel

--- src/components/ImageGallery/ImageGallery.js
+++ src/components/ImageGallery/ImageGallery.js
@@ -1,11 +1,9 @@
 import React from "react";
+import "./ImageGallery.css";
+
 import { Carousel } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "./ImageGallery.css";
 import TeachersDay from "../../assets/ImageGalleryPictures/TeachersDay.jpg";
-import IndependenceDay1 from "../../assets/ImageGalleryPictures/IndependenceDay1.JPG";
-import IndependenceDay2 from "../../assets/ImageGalleryPictures/IndependenceDay2.JPG";
-import IndependenceDay3 from "../../assets/ImageGalleryPictures/independenceDay3.JPG";
 import Expo from "../../assets/ImageGalleryPictures/Expo.JPG";
 import imagegallery from "../../assets/ImageGalleryPictures/imageGallery6.jpg";
 import image7 from "../../assets/ImageGalleryPictures/Image7.jpg";
@@ -13,16 +11,27 @@ import image1 from "../../assets/ImageGalleryPictures/image1.jpg";
 import image8 from "../../assets/ImageGalleryPictures/image8.jpg";
 import image9 from "../../assets/ImageGalleryPictures/image9.jpg";
 
+import Rimage10 from "../../assets/ImageGalleryPictures/Rimage10.jpg";
+import Rimage11 from "../../assets/ImageGalleryPictures/Rimage11.jpg";
+import Rimage12 from "../../assets/ImageGalleryPictures/Rimage12.jpg";
+import Rimage13 from "../../assets/ImageGalleryPictures/Rimage13.jpg";
+import Rimage14 from "../../assets/ImageGalleryPictures/Rimage14.jpg";
+
+
+
+
 
 const images = [
+  Rimage13,
+  Rimage11,
+  Rimage12,
+  Rimage14,
+  Rimage10,
   image9,
   image8,
   image1,
   TeachersDay,
   image7,
-  IndependenceDay1,
-  IndependenceDay2,
-  IndependenceDay3,
   Expo,
   imagegallery,
 ];
@@ -31,7 +40,7 @@ function ImageGallery() {
   return (
     <div className="mainDivIG">
       <div className="mainImageGallery">
-        <Carousel fade> 
+        <Carousel fade>
           {images.map((image, index) => (
             <Carousel.Item key={index}>
               <img
