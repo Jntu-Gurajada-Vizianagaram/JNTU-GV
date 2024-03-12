d32cd47abcd1bc27b2cf2fa3c8b3970663db9819
Haneesh Bandaru <97587849+haneesh-bandaru@users.noreply.github.com>
2024-03-12 03:33:41 +0000

    updated contact us

--- src/components/MainPage/Header/Contact-Us/ContactUs.js
+++ src/components/MainPage/Header/Contact-Us/ContactUs.js
@@ -27,7 +27,7 @@ function ContactUs() {
     <div className="contact-page">
       {/* College Section */}
       <div className="contact-details">
-        <RoundedPaper elevation={4} sx={{ lineHeight: "3" }}>
+        {/* <RoundedPaper elevation={4} sx={{ lineHeight: "3" }}>
           <CallIcon fontSize="large" color="primary" />
           <Typography variant="h6">Contact Phone Number</Typography>
           <Typography variant="p"> 08922 277388</Typography>
@@ -38,19 +38,49 @@ function ContactUs() {
           <Typography variant="h6">Contact Phone Number</Typography>
           <Typography variant="p"> vc@jntugv.edu.in</Typography>
           <Typography variant="p"> registrarpeshi@jntugv.edu.in</Typography>
-          {/* <Typography variant="p"> 08922 294316</Typography> */}
+        </RoundedPaper> */}
+        <RoundedPaper elevation={4} className="paper-details">
+          <Typography variant="h6">
+            <strong>Vice Chancellor Peshi</strong>{" "}
+          </Typography>
+          <Typography variant="p">
+            <CallIcon fontSize="medium" color="primary" /> 08922 222606{" "}
+          </Typography>
+          <Typography variant="p">
+            <EmailIcon fontSize="medium" color="primary" /> ps2vc@jntugv.edu.in
+          </Typography>
+          <Typography variant="p">
+            <EmailIcon fontSize="medium" color="primary" /> pa2vc@jntugv.edu.in
+          </Typography>
         </RoundedPaper>
-        <RoundedPaper elevation={4}>
-          <PlaceIcon fontSize="large" color="primary" />
-          <Typography variant="h6">Our Location</Typography>
-          <Typography>
+        {/* Member 2 */}
+        <RoundedPaper elevation={4} className="paper-details">
+          <Typography variant="h6">
+            <strong>Registrar Peshi</strong>{" "}
+          </Typography>
+          <Typography variant="p">
+            <CallIcon fontSize="medium" color="primary" /> 08922 294316
+          </Typography>
+          <Typography variant="p">
+            <EmailIcon fontSize="medium" color="primary" />{" "}
+            registrarpeshi@jntugv.edu.in
+          </Typography>
+          <Typography variant="p">
+            <EmailIcon fontSize="medium" color="primary" />{" "}
+            pa2registrarjntugv@gmail.com
+          </Typography>
+        </RoundedPaper>
+        <RoundedPaper elevation={4} className="paper-details">
+          <span style={{ display: "flex", alignItems: "center" }}> <PlaceIcon fontSize="medium" color="primary" />
+            <Typography variant="h6">  <strong>Our Location</strong></Typography> </span>
+          <Typography variant="p">
             JNTU-Gurajada College of Engineering Vizianagaram, Nh 43, Dwarapudi
             (Post), Vizianagaram, Andhra Pradesh, Andhra Pradesh 535003 · ~4.8
             km
           </Typography>
         </RoundedPaper>
       </div>
-      
+
       <iframe
         title="JNTU Vizianagaram Campus Map"
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3791.2779091876973!2d83.37309837464375!3d18.151107380356674!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3a3bef0baf9f2a11%3A0xdb0b518115b27e07!2sJNTU-GV%20COLLEGE%20OF%20ENGINEERING%20VIZIANAGARAM!5e0!3m2!1sen!2sin!4v1705165640046!5m2!1sen!2sin"
@@ -63,9 +93,8 @@ function ContactUs() {
       ></iframe>
 
       {/* Additional Contact Section */}
-      <div className="additional-contact-section ">
+      {/* <div className="additional-contact-section ">
         <div className="contact-cards">
-          {/* Member 1 */}
           <RoundedPaper elevation={2} className="paper-details">
             <Typography variant="h6">
               <strong>Vice Chancellor Peshi</strong>{" "}
@@ -80,7 +109,6 @@ function ContactUs() {
               <EmailIcon fontSize="medium" color="primary" /> pa2vc@jntugv.edu.in
             </Typography>
           </RoundedPaper>
-          {/* Member 2 */}
           <RoundedPaper elevation={2} className="paper-details">
             <Typography variant="h6">
               <strong>Registrar Peshi</strong>{" "}
@@ -99,7 +127,6 @@ function ContactUs() {
             </Typography>
           </RoundedPaper>
 
-          {/* Member 3 */}
           <RoundedPaper elevation={2} className="paper-details">
             <Typography variant="h6">
               <strong>Controller of Examination Office</strong>{" "}
@@ -113,7 +140,7 @@ function ContactUs() {
             </Typography>
           </RoundedPaper>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
