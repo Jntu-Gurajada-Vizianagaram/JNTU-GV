d5bca3c74a4417779421a3c085bb38f146370750
Tarunc224 <tarunchintada2004@gmail.com>
2024-07-16 16:01:21 +0530

    added new syllabus

--- src/components/MainPage/NavBar/Academics/Syllabus/Syllabus.js
+++ src/components/MainPage/NavBar/Academics/Syllabus/Syllabus.js
@@ -23,31 +23,85 @@ const data = {
           sno: 1,
           branch: "Civil Engineering",
           pdfLink:
-            "https://drive.google.com/file/d/1hEGLoAX6trn7qoiMNSYvnR960HMJHT_G/view?usp=sharing",
+            "https://drive.google.com/file/d/1v3YoFxv3nvjef-1CBPxnCHfAJP4CEboO/view?usp=sharing",
         },
+
         {
           sno: 2,
-          branch: "Mechanical Engineering and Automobile Engineering",
+          branch: "Electronics and Communication Engineering",
           pdfLink:
-            "https://drive.google.com/file/d/1rg6W0T7tVHAFhoDfcfZs499QA_hhCt9B/view?usp=sharing",
+            "https://drive.google.com/file/d/1YJdaXP-4ywSob7Q3GpBKRlOXeleNi4ny/view?usp=sharing",
         },
+        
         {
           sno: 3,
           branch: "Electrical and Electronic Engineering",
           pdfLink:
-            "https://drive.google.com/file/d/1Jgx9-JWH5DP5GIPtf8KOSdf7Iyvg6Tg5/view?usp=sharing",
+            "https://drive.google.com/file/d/1D0ul690rNQgz3hMEaEPcx83aq-abnkeN/view?usp=sharing",
         },
+       
+        
         {
           sno: 4,
-          branch: "Electronics and Communication Engineering",
+          branch: "Mechanical Engineering and Automobile Engineering",
           pdfLink:
-            "https://drive.google.com/file/d/1vr1-14rHGB-LIhzDzSIlJgZBS0OL_tp5/view?usp=sharing",
+            "https://drive.google.com/file/d/14gdUrWlCAHHJHwtRmYkjET59AWbP1EaY/view?usp=drive_link",
         },
         {
           sno: 5,
-          branch: "Computer Science and Engineering and Information Technology",
+          branch: "Computer Science and Engineering (Internet of Things)",
           pdfLink:
-            "https://drive.google.com/file/d/1TKfA85Uwqj3VIaRHu5VnaacDLeT-TxlR/view?usp=sharing",
+            "https://drive.google.com/file/d/1JDY5M4GK_NUb8MKIWi07BJuonQwKXFr4/view?usp=drive_link",
+        },
+        {
+          sno: 6,
+          branch: "Artificial Intelligence & Machine Learning",
+          pdfLink:
+            "https://drive.google.com/file/d/1PTTL7B3o39WR07hvrCcgBkayX9bT2XoZ/view?usp=drive_link",
+        },
+       
+        {
+          sno: 7,
+          branch: "Artificial Intelligence & Data Science",
+          pdfLink:
+            "https://drive.google.com/file/d/1Nytz4jctxjKVyOUM1UAiO3YHhOmmGFka/view?usp=drive_link",
+        },
+
+        {
+          sno: 8,
+          branch: "Computer Science and Engineering (Data Science)",
+          pdfLink:
+            "https://drive.google.com/file/d/1Nytz4jctxjKVyOUM1UAiO3YHhOmmGFka/view?usp=drive_link",
+        },
+        {
+          sno: 9,
+          branch: "Computer Science and Engineering (IOT and Cyber security including BCT)",
+          pdfLink:
+            "https://drive.google.com/file/d/1gsdaPVZ0iPD8vBHAvNk4YE6f6RuQo8Tr/view?usp=drive_link",
+        },
+        {
+          sno: 10,
+          branch: "Computer Science and Engineering (Cyber security)",
+          pdfLink:
+            "https://drive.google.com/file/d/1rInHyz4Y0Jv34NIqgQTiml-nDPLzxx6S/view?usp=drive_link",
+        },
+        {
+          sno: 11,
+          branch: "Computer Science and Engineering (Artificial Intelligence & Machine Learning)",
+          pdfLink:
+            "https://drive.google.com/file/d/1AwmEn9rFKPXzAA61pPFzDQKBBfxe7jIu/view?usp=drive_link",
+        },
+        {
+          sno: 12,
+          branch: "Computer Science and Engineering",
+          pdfLink:
+            "https://drive.google.com/file/d/1ZE-YD-by4-nM5JaD5fA-c_YBzbW6bCbd/view?usp=drive_link",
+        },
+        {
+          sno: 13,
+          branch: "Information Technology",
+          pdfLink:
+            "https://drive.google.com/file/d/1HeLmB5gohd1Zohz4HVjWBnH2DKIkfb_v/view?usp=drive_link",
         },
       ],
     },
