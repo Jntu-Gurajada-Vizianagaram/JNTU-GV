9d583b3e73a84e330d947e6814b8756ea3a14079
Tarunc224 <tarunchintada2004@gmail.com>
2024-07-18 19:33:55 +0530

    updated VC

--- src/components/MainPage/NavBar/Administration/ViceChancellor.js
+++ src/components/MainPage/NavBar/Administration/ViceChancellor.js
@@ -1,5 +1,5 @@
 import React from "react";
-import vc from "../../../../assets/AdminDirectoratesImages/vc.png";
+import vc from "../../../../assets/AdminDirectoratesImages/doe.png";
 import "./AdminandDirectorates.css"; // Import your CSS file here
 import { MdOutlineMailOutline } from "react-icons/md";
 function ViceChancellor() {
@@ -9,83 +9,47 @@ function ViceChancellor() {
         <div className="profile-Image">
           <img src={vc} alt="Profile" />
         </div>
-        <h3>Prof.Dr.K.Venkatasubbaiah</h3>
-        <h4>Hon’ble Vice-Chancellor</h4>
+        <h3>Dr. D. Rajya Lakshmi</h3>
+        <h4>Hon’ble Vice-Chancellor(i/c)</h4>
         <p>
           <MdOutlineMailOutline width="40px" />
           vc@jntugv.edu.in
         </p>
       </div>
       <div className="profileDetailsVC">
-        <p>
-          Dr. Venkata Subbaiah Kambagowni is the founder Vice-Chancellor of
-          Jawaharlal Nehru Technological University, Gurajada, Vizianagaram. He
-          is an academician par excellence, a beloved teacher, and a focused
-          leader with the vision to improve Indian education to internationally
-          approved standards.
-        </p>
-        <p>
-          Sr. Prof. Venkata Subbaiah Kambagowni comes with ~37 years of
-          experience in academia, during which he has a proven track record of
-          taking up various leadership positions. He served as the Chairman,
-          Faculty of Engineering, Dean (Research & Development), Dean
-          (Transport) and Honorary Director, AU Development Centre, Andhra
-          University. At the Department level, he served as the Head of the
-          Department and Chairman, Board of Studies in Mechanical Engineering,
-          AU College of Engineering, Andhra University. He also served as the
-          Academic Senate Member for Andhra University and Adikavi Nannaya
-          University. He served as Member of Bureau of Indian Standards
-          Committee and Governing Body Member as A. P. State Government Nominee
-          for 5 Engineering Colleges.
-        </p>
-        <p>
-          Prof. Kambagowni has an impressive track record of quality teaching,
-          innovation and research in the area of Mechanical Engineering, with a
-          'h' index of 25, citation index of 2242. He guided 29 Ph.Ds and 132
-          students for their M.E/M.Tech degrees. He published 235 research
-          papers in various national and international journals of repute and
-          presented his work at International Conferences in USA and UK. He also
-          has 3 patents published and a copyright granted. He served as the
-          Chief Editor, Journal of Manufacturing Technology and Management and
-          the Consulting Editor for Journal of Mechanical Engineering, The
-          Institution of Engineers (India). His areas of interests are Fuzzy
-          Systems, Supply Chain Management, Inventory Management, Quality
-          management, Composite materials and Optimization.
-        </p>
-        <p>
-          In recognition of his academic brilliance and innovative research, he
-          received State Best Teacher Award, Govt. of Andhra Pradesh in 2014,
-          Dr. Sarvepalli Radhakrishnan Award for Best Academician in 2008 and
-          Best Researcher Award in 2014 by the Andhra University. In addition to
-          this, he is a recipient of Best Paper Award in Marine Engineering
-          Division from The Institution of Engineers (India), 2017 & 2019 and
-          two national awards from the Indian Institute of Industrial
-          Engineering in 1992 and 1995. He received Technology Excellence Award
-          in recognition of his contributions and achievement in the field of
-          Engineering Education. He also received Col G N Bajpai Institution
-          Prize, The Institution of Engineers (India), Kolkata in 2022, Corps of
-          Engineers Prize, The Institution of Engineers (India), Kolkata in
-          2021, Engineer of the year Award in 2013 and Bharat Ratna Sir
-          Mokshagundam Visvesvaraya Award in 2014 from Government of Andhra
-          Pradesh and The Institution of Engineers (India), Andhra Pradesh State
-          Centre.
-        </p>
-        <p>
-          He is a part of various professional bodies like Institution of
-          Engineers (India), AP Academy of Sciences, Indian Institution of
-          Production Engineers at the Fellow level. He is also a member of
-          International Association of Engineers, ISHRAE, ISTE, ISTD, ISME,
-          ORSI, ISPS and CMSI. At the Institution of Engineers (India), he is a
-          Council Member and served as the Vice-President (2020-22). He also
-          held positions of office as Chairman, CATE, Chairman, Board of
-          Examinations of IEI, Chairman IEI, Andhra Pradesh State Centre,
-          Chairman and Honorary Secretary IEI, Visakhapatnam Local Centre,
-          National Board of Governors, ISHRAE, Delhi and President, ISHRAE
-          Visakhapatnam. He served as Chairman and Honorary Secretary from 2003
-          to 2007 of The Institution of Engineers (India), Visakhapatnam Local
-          Centre. Presently he is a council member of ISTE & AP Academic
-          Sciences and Member in Advisory Board, ISTD Vizag Chapter.
-        </p>
+      <p>
+            Dr. D. Rajya Lakshmi obtained her BTech in Electronics and
+            Communications Engineering from Andhra University, M.Tech in
+            Computer Science from Andhra University, and PhD from JNTU
+            Hyderabad, India. She is currently working as Professor in the
+            Department of Computer Science and Engineering at JNTUGV,
+            Vizianagaram, India.
+          </p>
+
+          <p>
+            Her research interests arevested in the area of Image Processing,
+            Machine Learning, Data Mining and Software Engineering. She accrued
+            a total teaching experience of around 29 years in which she found
+            her calling with academia and is pursuing her passion serving in
+            various capacities as Professor,Principal, Vice Principal Academics,
+            HOD, CSE & IT,BoSmemberBoS Chairperson, Academic Council Member,
+            Regional Co-Ordinator for APSLPRB, Co-Ordinator for campus wide
+            network, Member Research Review Committee at Department of CSE,
+            JNTUK, Co-Ordinator for AP POLYCET, APEMACET, Member, CAC, INTUK,
+            UCEN&UCEKfive emerging courses.
+          </p>
+          <p>
+            She has more than 75 research publications in various National,
+            International Journals and Conferences, Book Chapters, Books
+            andPatents published to her credit. With over 29 years of
+            experience, Dr. D. Rajya Lakshmi is well reputed for her Guest
+            Lectures on various topics spanning from Image Processing to Career
+            Advancement and is widely known for her commitment to teaching and
+            catering to students needs at large.She guided 13 students to
+            completion of their Ph.D and is guiding08 Ph.D students. Dr. Rajya
+            Lakshmi is known for her unique initiatives like Women Empowerment
+            and makes her mark in the roles that she adorns.
+          </p>
       </div>
     </div>
   );
