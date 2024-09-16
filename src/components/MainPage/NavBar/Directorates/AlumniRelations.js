9071ba91caa1cc1412138d57e2324d5a0078e482
HarshaAndrothu3 <21VV1A1203@jntugvcev.in>
2024-09-16 20:57:40 +0530

    updated

--- src/components/MainPage/NavBar/Directorates/AlumniRelations.js
+++ src/components/MainPage/NavBar/Directorates/AlumniRelations.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "../Administration/AdminandDirectorates.css"; // Import your CSS file here
-import daa from "../../../../assets/AdminDirectoratesImages/daa.jpeg";
+import daa from "../../../../assets/AdminDirectoratesImages/daa.jpg";
 
 
 function AlumniRelations() {
@@ -24,14 +24,14 @@ function AlumniRelations() {
         </div>
         <center>
           <div className="profileName">
-            <h3 className="ADh3">Dr. R. Rajeswara Rao</h3>
+            <h3 className="ADh3">Dr. K. Srikumar</h3>
             <h3 className="ADh3">
               Director of Alumni Relations
             </h3>
           </div>
           <div className="profileDesignation">
             <h4 className="ADh4">
-              Professor of Dept. of Computer Science and Engineering
+              Professor of Dept. of Electrical & Electronics Engineering
             </h4>
           </div>
         </center>
@@ -39,57 +39,70 @@ function AlumniRelations() {
         {/* Profile description */}
         <p>Email: daa@jntugv.edu.in</p>
         <hr></hr>
+        <p className="ADsub">Administrative Positions</p>
         <p>
-          Dr. Ramisetty Rajeswara Rao is presently working as Director(i/c)
-          Academic Audit & Director(i/c) Alumni Relations, JNTU-GV,
-          Vizianagaram. Dr. Rao completed his B. Tech in CSE in the year 1999
-          from V.R. Siddhartha Engineering College, Vijayawada. M.Tech in CSE
-          from JNTUH-Hyderabad in the year 2003, PhD in CSE from JNTUH-Hyderabad
-          in the year 2010. Dr. Rao pursued his Post Doc in University of
-          Missouri, Colombia, USA in the year 2019.. Dr. Rao is having 20 +
-          Years of Teaching Experience in the field of Computer Science and
-          Engineering. Dr. Rao served as officer in charge of Hostels and Head
-          of the Department, Vice Principal and Principal(i/c) JNTUK, UCEV,
-          Vizianagaram. JNTUK University College of Engineering, Vizianagaram
-          got NBA accreditation for CSE, ECE and Mechanical for 3 years during
-          his leadership as NBA Co-coordinator. He also served the institution
-          as IQAC Co-Ordinator. He authored one monograph titled with{" "}
-          <strong>
-            “Automatic Text Independent Speaker Recognition using Source
-            Feature”
-          </strong>{" "}
-          (Lap LABERT Publishing GmbH Co. KG, Germany) in the year 2012 and Two
-          Text-Books titled{" "}
-          <strong>
-            “Cloud Computing and virtualization” (BSP Publications)
-          </strong>{" "}
-          in the year 2014 and Software Engineering-A New Approach in the year
-          2018. Dr. Rao is Expert Visit Committee Member to{" "}
-          <strong>AICTE, New Delhi</strong>. He is a state level committee
-          member for Curriculum Development for the{" "}
-          <strong>
-            state of Andhra Pradesh appointed by AP-State Council for Higher
-            Education (APSCHE)
-          </strong>{" "}
-          for Computer Science & Engineering. Eight students have received{" "}
-          <strong>
-            Ph. D degree under his guidance from JNTUH-Hyderabad and
-            JNTU-Kakinada
-          </strong>
-          . To his credit he had published papers in{" "}
-          <strong>ACM, ELSEVIER, SPRINGER</strong> and other reputed journals.
-          He authored 86 Journals, presented papers in 35 conferences and
-          chaired 23 session chairs and gave 43 invited talks in various reputed
-          colleges in Andhra Pradesh and Telengana. Prof. Rao received{" "}
-          <strong>Best Researcher</strong> Award from JNUK, Kakinada on 28th
-          December, 2018. He received <strong>VIDYA RATAN</strong> award from
-          T.E.H.E.G, New Delhi for the year 2011. Dr. Rao is recipient of Jyesta
-          Acharya Award from Bharat Education Excellence Awards, Visakhapatnam,
-          India. He is an academic advisor to National Cyber Safety and Security
-          Standards (NCSSS). He is a Member of CSI and Sr. Member of IEEE. His
-          Areas of Interest are Artificial Intelligence, Speech Processing,
-          Pattern Recognition, NLP and Cloud Computing.
+          ❖ Presently performing duties as Director of Alumimi Relationship of JNTU GV from 14-08-2024.
         </p>
+        <p>
+          ❖ Performed duties as Principal of JNTU GV, College of Engineering Vizianagaram from 03-8-2023 to 13-08-2024.
+        </p>
+        <p>
+          ❖ Performed duties as Principal i/c of JNTU GV, College of Engineering Vizianagaram from 22-8-2022 to 03-08-2023.
+        </p>
+        <p>
+          ❖ Performed duties as Head of the EEE Department, UCEK(A),JNTUK, Kakinada Jan 2020 to June 2022
+        </p>
+        <p>
+          ❖ Performed duties as Project Engineer, JNTU GV College of Engineering Vizianagaram.
+        </p>
+        <p>
+          ❖ Performed duties as Additional Controller of Examinations, JNTUK  from 29-11-2017 to 30-04-2019
+        </p>
+        <p>
+          ❖ Performed duties as JNTUK NSS Coordinator from March, 2013 to March,2017.
+        </p>
+        <p>
+          ❖ Worked in several confidential exams as Member in EAMCET, ECET etc.
+        </p>
+        <p>
+          ❖ Worked as Head of the Department for EEE in JNTUK, Vizianagaram Campus for a period of 3 years
+        </p>
+        <p>
+          ❖ Performed duties as Officer In charge of Exams at JNTUK, Vizianagaram Campus.
+        </p>
+        <p>
+          ❖ With the support and guidance of  Senior Professors  in University and College, Electrical circuits Lab, Electrical Measurements Lab, Power Electronics lab, Electrical Machines - I & II Labs were established @ UCEV, Vizianagaram Campus during my presence as a faculty and Head of the EEE Dept.
+        </p>
+      
+        <p className="ADsub">Awards:</p>
+        <p>
+          ❖ Received VIJ trust presented Global Eminent Academician Award for the year 2021.
+        </p>
+
+
+        <p className="ADsub">National / International Conference Publication:</p>
+        <p>
+          ❖ Power flow analysis in the presence of VSM of Interline Power Flow Controller in an optimal location.
+        </p>
+        <p>
+          ❖ Multi- objective optimized voltage stability using NDSPSO with cost and loss objectives pp-50.
+        </p>
+        <p>
+          ❖ Incorporation of UPFC transformer model in an optimal location using sensitivity analysis.
+        </p>
+        <p>
+          ❖ Optimization of cost and voltage stability index using NDSPSO for estimation of loadability.
+        </p>
+        <p>
+          ❖ Multi-objective loadability enhancement with optimal SVC placement through risk index and modal analysis approaches.
+        </p>
+        <p>
+          ❖ Analysis of power system in the presence of Hybrid Flow Controller.
+        </p>
+        <p>
+          ❖ Multi-Objective optimization in the presence of Hybrid Flow Controller.
+        </p>
+
       </div>
     </div>
   );
