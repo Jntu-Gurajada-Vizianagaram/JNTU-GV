a04021f43e79c67f0d09d8571697efd3ba52201d
AnilSinthu73 <21vv5a1273@jntugvcev.in>
2025-02-10 17:05:01 +0530

    Youtube vedio play fix

--- src/components/HomePage/Youtube/Youtube.js
+++ src/components/HomePage/Youtube/Youtube.js
@@ -1,6 +1,6 @@
+import { Container, Grid } from '@mui/material'; // Using Material-UI components
 import React from 'react';
 import './Youtube.css'; // Import the CSS file
-import { Container, Grid } from '@mui/material'; // Using Material-UI components
 
 const Youtube = () => {
   return (
@@ -9,7 +9,11 @@ const Youtube = () => {
         {/* First YouTube Live Video */}
         <Grid item xs={12} sm={6}>
           <div className="youtube-video">
-          <iframe width="560" height="315" src="https://www.youtube.com/embed/qMmk5l2kjeE?si=MdR76d27y7OIkcO6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe> </div>
+          <iframe width="560" height="315" src="https://www.youtube.com/embed/qMmk5l2kjeE?si=MdR76d27y7OIkcO6" title="YouTube video player" 
+          frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+           referrerpolicy="strict-origin-when-cross-origin" 
+           allowfullscreen
+           ></iframe> </div>
         </Grid>
 
         {/* Second YouTube Live Video */}
@@ -20,8 +24,9 @@ const Youtube = () => {
               height="315"
               src="https://www.youtube.com/embed/4HOC6P8N28Q?si=X9TOjU5v3G4tZbdZ"
               title="YouTube live video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              frameborder="0"
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture;web-share"
+              referrerPolicy='strivt-origin-cross-origin"'
               allowFullScreen
             ></iframe>
           </div>
