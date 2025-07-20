fc8e822dd9e332e8c61c3d4bfcec45db38138bc5
HarshaAndrothu3 <21VV1A1203@jntugvcev.in>
2025-07-20 20:43:09 +0530

    updated iqac

--- src/components/MainPage/NavBar/Directorates/IQAC.js
+++ src/components/MainPage/NavBar/Directorates/IQAC.js
@@ -1,174 +1,102 @@
 import React from "react";
-import "../Administration/AdminandDirectorates.css"; // Import your CSS file here
-import dap from "../../../../assets/AdminDirectoratesImages/dap.jpg";
-
+import "../Administration/AdminandDirectorates.css";
+import dap from "../../../../assets/AdminDirectoratesImages/diqac.jpg";
 
 function IQAC() {
   return (
     <div className="mainContainer">
       <div className="allRightContent">
-        <div className="allRightContentHeading">Director of Internal Quality Assurance Cell</div>
-        <br></br>
+        <div className="allRightContentHeading">
+          Director of Internal Quality Assurance Cell
+        </div>
+        <br />
         <div className="allRightContentProfile">
-          {/* Profile information */}
           <div className="ADimg">
             <center>
-              <img
-                src={dap}
-                alt="Profile"
-              />
+              <img src={dap} alt="Profile" />
             </center>
           </div>
         </div>
         <center>
           <div className="profileName">
-            <h3 className="ADh3">Dr. K. Chandra Bhushana Rao</h3>
+            <h3 className="ADh3">Dr. S Kalesha Vali</h3>
             <h3 className="ADh3">
               Director of Internal Quality Assurance Cell
             </h3>
           </div>
           <div className="profileDesignation">
             <h4 className="ADh4">
-              Professor of Dept. of Electronics and Communication Engineering
+              Professor of Mathematics in the department of BS&HSS
             </h4>
           </div>
         </center>
         <hr />
-        {/* Profile description */}
         <p>Email: dap@jntugv.edu.in</p>
-        <hr></hr>
+        <hr />
         <p>
-          Dr. Kota. Chandrabhushana Rao, completed his B.E, M.E and PhD in ECE
-          from AU, Visakhapatnam during1992, 1995 and 2005 respectively. Having
-          experience of 29 years in teaching, research and administration of
-          engineering education. Worked as Principal, Dean, Professor and HoD in
-          reputed private institutions during 1992-2012 in A.P. At present,
-          working as a Professor and HoD of ECE at UCEV-JNTUK-Vizianagaram, A.P.
+          Dr. S Kalesha Vali is currently working as Professor of Mathematics in the department of BS&HSS, OSD(i/c) to the Hon’ble Vice-Chancellor and Director(i/c), IQAC at JNTUGV, Vizianagaram. Received his M.Sc., M.Phil., Ph. D degrees in Mathematics from Andhra University, Visakhapatnam. He is having 28 years of teaching and Research experience in reputed Engineering Colleges/Universities. He received Two medals and Two awards. Published more than 50 research papers in reputed Journals and published 2 Patents. Guided 5 Ph.Ds 2 M. Phils and guiding 12 Ph.Ds. Completed one funded research project and authored two textbooks.
         </p>
 
-        
-        <p className="ADsub">Achievements:</p>
-        <p>❖ Senior member, IEEE since 2016.</p>
-        <p>❖ Professor of ECE since Jan.2016.</p>
-        <p>❖ HoD of ECE –UCEV since Feb.2017.</p>
-        <p>❖ Published TWO Indian Patents; TWO copy rights;</p>
-        <p>
-          ❖ Published 67 research papers, in reputed Journals and conference
-          proceedings.
-        </p>
-        <p>
-          ❖ Guided TWO PhDs and seven scholars are working for PhD at present
-        </p>
-        <p>
-          ❖ Visited UK and Thailand to present research papers and chaired
-          technical sessions.
-        </p>
-        <p>❖ Chaired several International conferences in India and abroad.</p>
-        <p>
-          ❖ Fellow of IETE, Life member-ISTE, Life member-SEMC(I), LM- ISSS(
-          IISc), Member- IAENG.
-        </p>
-        <p>
-          ❖ Research board member, Anna university, Vignan university, Integral
-          university.
-        </p>
-        <p>❖ Chairman, BoS, ECE –UCEV-JNTUK since 2017.</p>
-        
-        <p className="ADsub">Recognitions:</p>
-        <p>❖ Evaluator- NBA since 2009.</p>
-        <p>❖ EVC-Member, AICTE and Evaluator of NBA;</p>
-        <p>❖ Obtained Level 5-CMI-UK- certificate.</p>
-        <p>
-          ❖ Having National recognitions and associations with NAAC, NBA, MSME,
-          CII, WIPRO, TCS andAICTE, learned and practiced quality SOPs to serve
-          higher education in India.
-        </p>
-        <p>
-          ❖ Identified by AICTE as one of the 200 participants across India for
-          UK-India EducationResearch Initiative (UKIERI) scheme and completed
-          CMI certification during 2015-16.
-        </p>
-        <p>❖ Visited DUDLEY college of UK-Birmingham on invitation.</p>
-        <p>❖ Expert member, RIFD Bureau of AICTE.</p>
-        <p>
-          ❖ GB member and BoS member in reputed autonomous colleges appointed by
-          JNTUK.
-        </p>
-        <p>❖ Coordinator for NAAC and NBA at UCEV-JNTUK during 2013-19.</p>
-        <p>
-          ❖ NBA and NAAC- Consultant and trainer-so far guided Fifteen
-          institutions successfully.
-        </p>
-        <p>❖ Resource person for ESCI-Hyderabad.</p>
-        <p>❖ Chairman- CPRC- SAMEER Visakhapatnam.</p>
-        
-        <p className="ADsub">Involvement in accreditations:</p>
-        <p>
-          ❖ Expert member of National Board of Accreditation (NBA) since 2011,
-          involved as member, with Mock-accreditation team for Washington Accord
-          (WA) regulations during Dec2012 withProf. Unnikrishnan, Advisor for
-          INDIA, Washington Accord, USA.
-        </p>
-        <p>
-          ❖ Visited more than 20-good institutions for accreditation, as expert
-          member of NBA so far.
-        </p>
-        <p>❖ Conducted Three OBA- OBE workshops of NBA at UCEV-JNTUK.</p>
-        <p>
-          ❖ Conducted several OBE-OBA workshops across India at Jammu,
-          Tamilnadu, MP, Maharashtra, Uttar Pradesh, Rajasthan, Haryana,
-          Telangana, AP and other states.
-        </p>
-        
-        <p className="ADsub">
-          Given guidance towards NBA (SAR) OBE-OBA and NAAC (new SSR) to
-        </p>
-        <p>❖ SRKR Engg.College, a 30 year old institution at Bhimavaram.</p>
-        <p>❖ Jayam COE, Dharmapuri, Tamilnadu.</p>
-        <p>❖ Sona COE, salem , Tamilnadu.</p>
-        <p>❖ Anna university COE, Trichy.</p>
-        <p>❖ K.J. Somaih COE, Mumbai.</p>
-        <p>
-          ❖ Learningware, winzest of Hyderabad towards Tier-1 and Tier-II
-          formats of NBA.
-        </p>
-        <p>❖ VIIT- and VIEW at duvvada for SAR preparation.</p>
-        <p>❖ Swarnandhra Group of colleges at Narsapur.</p>
-        <p>❖ Vignan university at Guntur for NBA-SAR preparation.</p>
-        <p>❖ VVIT-Guntur for SAR Preparation and SSR preparation.</p>
-        <p>❖ GVP college of Engineering for Women, Visakhapatnam.</p>
-        <p>
-          ❖ NBA and NAAC guidance has been given to many other reputed
-          engineering colleges in A.P., Telangana, Maharashtra, Madhya Pradesh,
-          Jammu, Uttar Pradesh and Tamilnadu.
-        </p>
-        <p>
-          ❖ Conducted OBE-OBA workshops at Ujjain College of Engineering, MP,
-          Bikaner College of Engineering- Rajasthan, Integral university,
-          Lucknow and at ESCI chapter-Jammu.
-        </p>
-        
-        <p className="ADsub">Other Achievements:</p>
-        <p>
-          ❖ Guided several students to acquire core jobs in ISRO, SAMEER, BSNL
-          and other reputed organizations.
-        </p>
-        <p>
-          ❖ Obtained good feedback from students at UCEV and other institutes
-          where I worked and taught.
-        </p>
-        <p>❖ Trained Contract faculty towards NBA, NAAC at UCEV.</p>
-        <p>❖ Established RUSA project facilities at ECE Department.</p>
-        <p>❖ Established SC/ST book bank at Library of UCEV during 2013-15.</p>
-        <p>❖ TCS MoU under TAAP is executed during 2015 as TPO at UCEV.</p>
-        <p>❖ Executed THREE MoUs with ECE department during 2020-21.</p>
-        <p>
-          ❖ Trained students through Virtual labs using mobile app- first of its
-          kind!
-        </p>
-        <p>
-          ❖ Given about 23 webinars as Resource person during 2020-21 Lock down.
-        </p>
+        <p className="ADsub">Teaching experience :</p>
+        <p>❖ Working as Professor of Mathematics at JNTUGV, Vizianagaram since June 2025.</p>
+        <p>❖ Worked as Professor of Mathematics at JNTUK UCEV, Vizianagaram/ JNTUK Kakinada/Andhra University, Visakhapatnam for 13 years.</p>
+        <p>❖ Worked as Associate Professor of Mathematics at GITAM University, Visakhapatnam for 5 years.</p>
+        <p>❖ Worked as Assistant Professor of Mathematics at GITAM University, Visakhapatnam / PVP Sidhartha Institute of Technology, Vijayawada for 8 years.</p>
+        <p>❖ Worked as Lecturer of Mathematics at Nimra College of Engineering and Technology, Vijayawada for 2 years.</p>
+
+        <p className="ADsub">Administration Contributions :</p>
+        <p>❖ Officer on Special duty (OSD)(i/c) to the Hon’ble Vice-Chancellor, JNTUGV, Vizianagaram since July 2025.</p>
+        <p>❖ Director (i/c), IQAC, JNTUGV, Vizianagaram since July 2025.</p>
+        <p>❖ Head, Department of BS&HSS, JNTUK University College of Engineering, Vizianagaram for 6 years.</p>
+        <p>❖ Officer In-Charge, Library, JNTUK University College of Engineering, Vizianagaram for 1 year.</p>
+        <p>❖ Convener, FFC visits to Engineering, MCA, Management and Pharmacy Colleges affiliated to JNTUK, JNTUGV since 2013.</p>
+        <p>❖ Member, College Academic Committee and Departmental Committee, JNTUK University College of Engineering, Vizianagaram for 10 years.</p>
+        <p>❖ Chief Coordinator, Spot Valuations, Andhra University, Visakhapatnam for 3 years.</p>
+        <p>❖ Nodal Officer, Examinations, Andhra University, Visakhapatnam for 1 year.</p>
+        <p>❖ Controller of Examinations, GITAM University, Visakhapatnam for 3 years.</p>
+        <p>❖ Addl. Controller of Examinations, GITAM University, Visakhapatnam for 1 year.</p>
+        <p>❖ Coordinator, GAT (GITAM Admission Test) admissions Counselling (online), GITAM University, Visakhapatnam for 2 years.</p>
+        <p>❖ Coordinator, Convocations, GITAM University, Visakhapatnam, first 3 Convocations.</p>
+        <p>❖ Head, Department of BS&HSS, PVP Sidhartha Institute of Technology, Vijayawada for 4 years.</p>
+        <p>❖ Head, Department of Mathematics, Nimra College of Engineering and Technology, Vijayawada for 2 years.</p>
+        <p>❖ Member, Expert Visit Committee to visit different Technical Institutions, AICTE, New Delhi.</p>
+        <p>❖ Member, Academic Affairs Committee, Discipline Committee, Departmental Committee, Proctorial Committee, Anti-Ragging Committee, Information Management (IMG) Committee, GITAM University, Visakhapatnam for 2 years.</p>
+
+        <p className="ADsub">Academic Contributions :</p>
+        <p>❖ Authored textbook ‘Engineering Mathematics-I’, Cengage Learning, 2018.</p>
+        <p>❖ Authored textbook ‘Differential equations and Vector Calculus’, Cengage Learning, 2025.</p>
+        <p>❖ One of the Reviewer of the Textbooks ‘Intermediate Mathematics’, IA, IB, IIA, IIB of Telugu & Sanskrit Academy of Andhra Pradesh during 2022–2024.</p>
+        <p>❖ Chairman, Board of Studies, BS&HSS, JNTUK University College of Engineering, Vizianagaram for 3 years.</p>
+        <p>❖ Member, Research Review Committee, Department of BS&HSS(Mathematics) JNTUGV, Vizianagaram since November 2024.</p>
+        <p>❖ Member, Research Review Committee, JNTUK Kakinada since 2013.</p>
+        <p>❖ Member, Board of Studies, JNTUK-Kakinada, JNTUA-Anantapur, Andhra University-Visakhapatnam, Dr. B R Ambedkar University-Srikakulam, UCEV-Vizianagaram, UCEK-Kakinada, GITAM University-Visakhapatnam and number of autonomous colleges in Andhra Pradesh.</p>
+
+        <p className="ADsub">Research Contributions :</p>
+        <p>❖ Areas of Research include Algebra, Lattice theory and Mathematical Modelling (Epidemic models).</p>
+        <p>❖ Awarded 5 Ph.Ds., 2 M.Phils., 12 Scholars are working for Ph. D at present.</p>
+        <p>❖ Published 2 patents.</p>
+        <p>❖ Published 52 papers in National/International journals, 12 papers presented in Conferences.</p>
+        <p>❖ Organised 01 Conference, 08 Workshops, attended workshops/Short term Courses/seminars 37, Chaired sessions 04 and delivered 06 talks.</p>
+        <p>❖ Visited Hong Kong to present a paper and deliver a talk in the International Conference in July 2016.</p>
+        <p>❖ Completed UGC funded research project of worth Rs 4.12 Lakhs.</p>
+        <p>❖ Consultancy carried out as technical team member for Greater Visakha Municipal Corporation (GVMC) at GITAM University, Visakhapatnam.</p>
+
+        <p className="ADsub">Professional Contributions :</p>
+        <p>❖ Life member, Andhra Pradesh Society for Mathematical Sciences (Regd. No. 1310/1986).</p>
+        <p>❖ Life member, Prof. Minakshi Sundaram Memorial Society (Regd. No. 103/2010).</p>
+        <p>❖ Served as Vice-President for Andhra Pradesh Society for Mathematical Sciences (Regd. No. 1310/1986) for 2 years.</p>
+
+        <p className="ADsub">Societal Contributions :</p>
+        <p>❖ Worked as Treasurer/ Secretary for Moulana Abul Kamal Azaad Trust (Regd. No: 78/2003) for 20 years.</p>
+        <p>❖ Member, Islamic Centre (Regd.No.58/1969), Visakhapatnam since 2015.</p>
+        <p>❖ Served as President, Teachers Association, UCEV (Regd.No.153/2013) for one year.</p>
+
+        <p className="ADsub">Academic Achievements :</p>
+        <p>❖ Academic Excellence Award-2019 by MAKAT on National Education Day 2019.</p>
+        <p>❖ Conferred with SOMASS Award for proficiency in Mathematics at Post Graduation Studies.</p>
+        <p>❖ Secured Certificate of Merit and a Medal issued by ROTARY CLUB International Rajahmundry, for higher proficiency in Post Graduation Studies.</p>
+        <p>❖ Won B.R.M. Memorial Medal for proficiency in Mathematics at School level.</p>
+        <p>❖ Stood College Topper at Degree and PG level.</p>
       </div>
     </div>
   );
