9f8c811b9df5bc8f2992505cd1dca1bfa9f051a8
HarshaAndrothu3 <21VV1A1203@jntugvcev.in>
2026-05-17 15:07:05 +0530

    last updated

--- src/components/MainPage/LastUpdated/LastUpdated.js
+++ src/components/MainPage/LastUpdated/LastUpdated.js
@@ -5,7 +5,7 @@ const LastUpdated = () => {
   return (
     <div className="last-updated-container">
       <p className="last-updated-text">
-        Last Updated: May 06 | 02:00 AM
+        Last Updated: May 17 | 03:00 AM
       </p>
     </div>
   );
